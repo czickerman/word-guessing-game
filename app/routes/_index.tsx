5031311351b1744d856b21946002cfae982044fd
Clayton Zickerman <cczickerman@gmail.com>
2024-03-02 00:16:42 -0500

    make website responsive to smaller displays

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -64,8 +64,8 @@ export default function Index() {
       <div className="opacity-1 animateMainContent">
         {showMainContent && (
           <div>
-            <div className="text-3xl flex flex-col items-start">
-              <h1 className="max-w-[75vh]">Hint: {wordFetcher.data?.meaning ?? "Loading"}</h1>
+            <div className="text-xl md:text-3xl flex flex-col items-start">
+              <h1 className="max-w-[75vw] md:max-w-[50vw]">Hint: {wordFetcher.data?.meaning ?? "Loading"}</h1>
               {showFirstLetter && <h1>First Letter: {wordFetcher.data?.firstLetter ?? "Loading"}</h1>}
               <GuessForm guessFetcher={guessFetcher} wordFetcher={wordFetcher} />
             </div>
